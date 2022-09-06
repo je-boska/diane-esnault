658476833e8e72aa517d423db3fa95f03f8572cd
je-boska <joneirikboska@gmail.com>
2022-09-06 12:26:45 +0200

    Initial commit with data fetching from Contentful

---
+++ src/queries/Post.ts
@@ -0,0 +1,49 @@
+export async function getPosts() {
+  const response = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${
+      import.meta.env.PUBLIC_CONTENTFUL_SPACE_ID
+    }`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${
+          import.meta.env.PUBLIC_CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      body: JSON.stringify({
+        query: `
+          query PostsQuery {
+              postCollection (limit: 10) {
+                  items {
+                      title
+                      slug
+                      body {
+                          json
+                          links {
+                            assets {
+                              block {
+                                sys {
+                                  id
+                                }
+                                contentType
+                                title
+                                description
+                                url
+                                width
+                                height
+                              }
+                            }
+                          }
+                      }
+                  }
+              }
+          }
+          `,
+      }),
+    }
+  );
+
+  const json = await response.json();
+  return json.data.postCollection.items;
+}
